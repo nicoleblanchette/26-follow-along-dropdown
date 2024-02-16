9d2faa65cd31be7de0c39a5884f6c01b98533b82
cyph404 <nicoleblanchette05@gmail.com>
2024-02-16 14:24:53 -0500

    can someone fix my hmr

---
+++ app/src/main.js
@@ -0,0 +1,37 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+const handleEnter = (e) => {
+  console.log('enter');
+  e.target.classList.add('trigger-enter');
+  setTimeout(() => {
+    if (e.target.classList.contains('trigger-enter'))
+      e.target.classList.add('trigger-enter-active');
+  }, 150);
+  background.classList.add('open');
+
+  const dropdown = e.target.querySelector('.dropdown');
+
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left
+  };
+
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+};
+
+const handleLeave = (e) => {
+  console.log('leave');
+  e.target.classList.remove('trigger-enter', 'trigger-enter-active');
+};
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
+
---
+++ app/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: { outDir: '..' },
+  base: '/26-follow-along-dropdown/', 
+});
